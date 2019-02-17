67399817469c9539bbf032951cd5a88ddec70a79
Nikolay Mladenov <nidenov@gmail.com>
2019-02-17 12:10:48 +0100

    Fucking egotistical bastard. adds expandtab to vimrc

---
+++ js/app.js
@@ -0,0 +1,52 @@
+
+let c_note_id = 0;
+let notes = [];
+
+function Note(title){
+    this.id = get_first_free_id();
+    this.title = title;
+    this.text = 'Lorem ipsum te';
+    this.tags = [ 'le', 're'];
+    var done = false;
+
+    this.describe = function(){
+        return 'Note:' + this.id + ', title:' + 
+            this.title + ', done:' + done +
+            ', tags: -' + this.tags +
+            '-, text:' + this.text; 
+    }
+
+    this.set_done = function(){
+        done = true;
+    }
+}
+
+function get_first_free_id(){
+    return notes.length
+}
+
+function c_note_done(){
+    notes[c_note_id].set_done();   
+    console.log('Setting current note as done!');
+}
+
+function create_note(title) {
+    var new_note = new Note(title); 
+    insert_new_note(new_note);
+}
+function insert_new_note(new_note) {
+    notes.push(new_note);
+}
+
+
+function go_back_note() {
+    console.log("Going back");
+}
+function go_for_note() {
+    console.log("Going forward");
+}
+
+var ex_note = new Note('First note');
+
+notes.push(ex_note); 
+console.log(ex_note.describe());
