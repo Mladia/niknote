34ead7a0d54a2c8075e1e5c810b384f7c0f1de71
Nikolay Mladenov <nidenov@gmail.com>
2019-02-22 19:47:27 +0100

    adding go bac and for

--- js/app.js
+++ js/app.js
@@ -235,3 +235,88 @@ function save_changed_note(){
     new_note_tags_field.value = "";
     new_note_text_field.text = "";
 }
+
+//Gyro
+window.addEventListener("deviceorientation", handleOrientation, true);
+var output = document.querySelector('.output');
+var in_alpha = 0;
+var in_beta = 0;
+var in_gamma = 0;
+var alpha = 0;
+var beta = 0;
+var gamma = 0;
+
+var first_flip_gb = false;
+var second_flip_gb = false;
+var first_flip_gf = false;
+var second_flip_gf = false;
+var first = true;
+var flipped_degree = 80;
+var margin_error = 15;
+
+function met(x, limit){
+    if ( Math.abs( x - limit ) < margin_error ){
+        return true;
+    }else{
+        false;
+    }
+}
+
+function check_go_back() {
+    if ( met(gamma, 85) && met(beta, 130) ) {
+        first_flip_gb = true
+        output.innerHTML += "1first!\n";
+    }
+    if (first_flip_gb && met(gamma, in_gamma) && met(beta, in_beta ) ){
+        second_flip_gb = true;
+        first_flip_gb = false;
+        output.innerHTML += "1second!\n";
+        go_back_note();
+    }
+}
+
+function check_go_for() {
+    if ( met(gamma, -85) && met(beta, 130) ) {
+        first_flip_gf = true
+        output.innerHTML += "2first!\n";
+    }
+    if (first_flip_gf && met(gamma, in_gamma) && met(beta, in_beta ) ){
+        second_flip_gf = true;
+        first_flip_gf = false;
+        output.innerHTML += "2second!\n";
+        go_for_note();
+    }
+}
+
+function handle_gestures(){
+    check_go_back();
+    check_go_for();
+
+}
+
+
+
+function handleOrientation(event) {
+    // var absolute = event.absolute;
+    alpha    = event.alpha;
+    beta     = event.beta;
+    gamma    = event.gamma;
+  
+
+    output.innerHTML = "";
+    output.innerHTML = "alpha: " + alpha + "\n";
+    output.innerHTML += "beta: " + beta + "\n";
+    output.innerHTML += "gamma: " + gamma + "\n";
+    output.innerHTML += "in_alpha: " + in_alpha + "\n";
+    output.innerHTML += "in_beta: " + in_beta + "\n";
+    output.innerHTML += "in_gamma: " + in_gamma+ "\n";
+
+    if (first) {
+        in_alpha = alpha;
+        in_beta = beta;
+        in_gamma = gamma;
+        first = false;
+    }
+
+    handle_gestures();
+}
